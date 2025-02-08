82cce433834ce80d98470d4fceed65878251e36c
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-02-09 01:30:18 +0200

    added assets for test & added route for movie details & html structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { MovieDetailsPageComponent } from './movie-details-page/movie-details-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: "movie/:id",
+    component: MovieDetailsPageComponent,
+    title: "Movie"
+  }
+
+];
