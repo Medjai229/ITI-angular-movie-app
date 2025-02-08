40702c9a4a1cdfc09ead30ddc818c8f3861ed34e
Test0-VC <omarabdin1@gmail.com>
2025-02-08 15:47:15 +0200

    Movie-Wishlist page

--- src/app/movie-wishlist-page/movie-wishlist-page.component.ts
+++ src/app/movie-wishlist-page/movie-wishlist-page.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { WishlistService } from '../services/wishlist.service';
+import { CommonModule } from '@angular/common';
+import { MovieCardComponent } from "../movie-card/movie-card.component";
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-movie-wishlist-page',
-  imports: [],
+  imports: [CommonModule, MovieCardComponent , RouterModule],
   templateUrl: './movie-wishlist-page.component.html',
   styleUrl: './movie-wishlist-page.component.css'
 })
-export class MovieWishlistPageComponent {
+export class MovieWishlistPageComponent implements OnInit {
+  wishlist: any[] = [];
 
+  constructor(private wishlistService: WishlistService) {}
+
+  ngOnInit() {
+    this.wishlist = this.wishlistService.getWishlist();
+  }
+
+  removeFromWishlist(movieId: number) {
+    this.wishlistService.removeFromWishlist(movieId);
+    this.wishlist = this.wishlistService.getWishlist();
+  }
 }
