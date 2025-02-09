2ac16ef16365cb30352bc845189d7ad224b10580
Mohamed Ahmed <mohamedismaildev@gmail.com>
2025-02-10 00:23:07 +0200

    complete movie details get data from api

--- src/app/movie-details-page/movie-details-page.component.ts
+++ src/app/movie-details-page/movie-details-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { RequestService } from '../services/request.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-movie-details-page',
-  imports: [],
+  imports: [DatePipe],
   templateUrl: './movie-details-page.component.html',
   styleUrl: './movie-details-page.component.css'
 })
@@ -14,6 +15,17 @@ export class MovieDetailsPageComponent {
   movie: any
 
   constructor(private RequestService: RequestService, private route: ActivatedRoute, private router : Router){}
+
+
+
+  getPoster(movie: any) {
+    return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
+  }
+
+
+  getLogo(movie: any) {
+    return `https://image.tmdb.org/t/p/w200/${movie.production_companies[0].logo_path}`;
+  }
 
 
   ngOnChange() {
