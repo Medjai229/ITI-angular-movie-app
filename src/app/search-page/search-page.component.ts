ab048b39258ba567b410b1f3396bf0f2168fc88f
Malik Hussein <malikhussein229@gmail.com>
2025-02-10 14:17:59 +0200

    Updated the search page to handle the redirection from the home page and to display the url with the search word and page

--- src/app/search-page/search-page.component.ts
+++ src/app/search-page/search-page.component.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { PaginationComponent } from '../pagination/pagination.component';
 import { PaginationService } from '../service/pagnation.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search-page',
@@ -15,7 +16,22 @@ export class SearchPageComponent {
   searchResults: any = [];
   totalPages: number = 0;
   currentPage: number = 1;
-  constructor(private requestService: RequestService) {}
+  constructor(
+    private requestService: RequestService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  ngOnInit() {
+    this.route.params.subscribe((params) => {
+      if (params['search']) {
+        console.log(params);
+        this.searchData = params['search'];
+        this.currentPage = +params['page'] || 1;
+        this.search();
+      }
+    });
+  }
 
   loadMovies(page: number): void {
     this.requestService
@@ -29,6 +45,7 @@ export class SearchPageComponent {
 
   onPageChange(page: number): void {
     this.loadMovies(page);
+    this.router.navigate(['/search', this.searchData, page]);
   }
 
   searchData: string = '';
@@ -37,9 +54,6 @@ export class SearchPageComponent {
   search() {
     this.loadMovies(this.currentPage);
     this.searchedQuery = this.searchData;
-
-    this.requestService
-      .getSearchResults(this.searchData, this.currentPage)
-      .subscribe((res: any) => (this.searchResults = res.results));
+    this.router.navigate(['/search', this.searchData, this.currentPage]);
   }
 }
