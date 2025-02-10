def776f1b73c0b1454ecd5792a80f906711345ad
Malik Hussein <malikhussein229@gmail.com>
2025-02-10 14:16:57 +0200

    Added the search bar that will redirect to the search page

--- src/app/movie-list-page/movie-list-page.component.ts
+++ src/app/movie-list-page/movie-list-page.component.ts
@@ -3,21 +3,25 @@ import { Component } from '@angular/core';
 import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { PaginationService } from '../service/pagnation.service';
 import { PaginationComponent } from '../pagination/pagination.component';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-movie-list-page',
-  imports: [MovieCardComponent, PaginationComponent],
+  imports: [MovieCardComponent, PaginationComponent, FormsModule],
   templateUrl: './movie-list-page.component.html',
   styleUrl: './movie-list-page.component.css',
 })
 export class MovieListPageComponent {
   constructor(
     private requestService: RequestService,
-    private _paginationService: PaginationService
+    private _paginationService: PaginationService,
+    private router: Router
   ) {}
   movies: any[] = [];
   totalPages: number = 0;
   currentPage: number = 1;
+  searchData: string = '';
 
   ngOnInit(): void {
     this.loadMovies(this.currentPage);
@@ -33,5 +37,9 @@ export class MovieListPageComponent {
 
   onPageChange(page: number): void {
     this.loadMovies(page);
+  }
+
+  search() {
+    this.router.navigate(['/search', this.searchData, 1]);
   }
 }
