ea72d9a9a0ed27cf6d679e47632d85ec271c65ff
Test0-VC <omarabdin1@gmail.com>
2025-02-10 16:02:52 +0200

    added real time counter for wishlist

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { WishlistService } from '../services/wishlist.service';
 
 @Component({
   selector: 'app-nav-bar',
@@ -7,6 +8,14 @@ import { RouterLink } from '@angular/router';
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
+export class NavBarComponent implements OnInit {
+  wishlistCount = 0;
 
+  constructor(private wishlistService: WishlistService) {}
+
+  ngOnInit() {
+    this.wishlistService.wishlistCount$.subscribe((count) => {
+      this.wishlistCount = count; // Updates wishlist count in navbar
+    });
+  }
 }
