1782961824e843c497661628b8b9e79c4f981847
Test0-VC <omarabdin1@gmail.com>
2025-02-10 16:02:13 +0200

    attached wishlist Service to heart button in card

--- src/app/movie-card/movie-card.component.ts
+++ src/app/movie-card/movie-card.component.ts
@@ -1,6 +1,7 @@
 import { DatePipe, DecimalPipe, NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { WishlistService } from '../services/wishlist.service';
 
 @Component({
   selector: 'app-movie-card',
@@ -10,6 +11,8 @@ import { RouterLink } from '@angular/router';
 })
 export class MovieCardComponent {
   @Input() movie: any;
+  constructor(private wishlistService: WishlistService) {}
+
 
   getPoster(movie: any) {
     return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`;
@@ -18,5 +21,17 @@ export class MovieCardComponent {
   handlePosterErr(e: Event) {
     const img: any = e.target;
     img.src = 'Image-not-found.png';
+  }
+  // wishList
+  toggleWishlist() {
+    if (this.isWishlisted()) {
+      this.wishlistService.removeFromWishlist(this.movie.id);
+    } else {
+      this.wishlistService.addToWishlist(this.movie);
+    }
+  }
+
+  isWishlisted(): boolean {
+    return this.wishlistService.isInWishlist(this.movie.id); // Checks if movie is in wishlist
   }
 }
