4e2d45b1a4a2d85289161181c6cba4bf150546a2
Test0-VC <omarabdin1@gmail.com>
2025-02-11 16:09:02 +0200

    added navigation to details page when clicking on movie in the watch list

--- src/app/movie-wishlist-page/movie-wishlist-page.component.ts
+++ src/app/movie-wishlist-page/movie-wishlist-page.component.ts
@@ -2,10 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../services/wishlist.service';
 import { CommonModule, DatePipe } from '@angular/common';
 import { MovieCardComponent } from "../movie-card/movie-card.component";
-import { RouterModule } from '@angular/router';
+import { RouterLink, RouterModule } from '@angular/router';
 @Component({
   selector: 'app-movie-wishlist-page',
-  imports: [CommonModule, MovieCardComponent , RouterModule,DatePipe],
+  imports: [CommonModule, MovieCardComponent , RouterModule,DatePipe,RouterLink],
   templateUrl: './movie-wishlist-page.component.html',
   styleUrl: './movie-wishlist-page.component.css'
 })
