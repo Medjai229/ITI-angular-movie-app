1e0952d4c8cc1e4a011d93a0c11fbfd1f4dc5db3
ahmed amr <ahmedamro3000@gmail.com>
2025-02-11 18:40:32 +0200

    new

--- src/app/movie-wishlist-page/movie-wishlist-page.component.ts
+++ src/app/movie-wishlist-page/movie-wishlist-page.component.ts
@@ -1,13 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { WishlistService } from '../services/wishlist.service';
 import { CommonModule } from '@angular/common';
-import { MovieCardComponent } from "../movie-card/movie-card.component";
+import { MovieCardComponent } from '../movie-card/movie-card.component';
 import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-movie-wishlist-page',
-  imports: [CommonModule, MovieCardComponent , RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './movie-wishlist-page.component.html',
-  styleUrl: './movie-wishlist-page.component.css'
+  styleUrl: './movie-wishlist-page.component.css',
 })
 export class MovieWishlistPageComponent implements OnInit {
   wishlist: any[] = [];
